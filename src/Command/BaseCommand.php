897cea6a22057a805de2d185b9df1224c3d8a6ae
Andy Giles <andyg5000@gmail.com>
2017-06-01 16:07:53 -0400

    Added config dependencies to BaseCommand.php

--- src/Command/BaseCommand.php
+++ src/Command/BaseCommand.php
@@ -9,10 +9,28 @@ use Symfony\Component\Yaml\Yaml;
 class BaseCommand extends Command
 {
   protected $config;
+  protected $dir;
+  protected $build_root;
 
   public function __construct($name = null) {
     parent::__construct($name);
+    $this->dir = getcwd();
     $this->setConfig();
+
+    // Verify temporary directory is defined and writable.
+    if (empty($this->config['temp']) || !file_exists($this->config['temp'])) {
+      throw new \Exception("The temp directory is not defined or does not exist in your config file.");
+    }
+
+    // Verify build root is defined and writable.
+    if (empty($this->config['build']['root']) || !file_exists($this->config['build']['root'])) {
+      throw new \Exception("The build root directory is not defined or does not exist in your config file.");
+    }
+
+    $this->build_root = $this->dir . '/' . $this->config['build']['root'];
+    if (!file_exists($this->build_root)) {
+      throw new \Exception("The build root specified in your config is not writable");
+    }
   }
 
   private function setConfig()
