152b2aff588d5899d34ea78b0b4b04e7d08bd181
Andy Giles <andyg5000@gmail.com>
2017-06-05 13:35:05 -0400

    Added options to Rsync service

--- src/Service/Rsync.php
+++ src/Service/Rsync.php
@@ -2,8 +2,14 @@
 
 namespace BOI_CI\Service;
 
+use Drupal\Driver\Exception\Exception;
+
 class Rsync extends Shell {
   private $rsync;
+  private $options;
+  private $flags = '-vr';
+  private $source;
+  private $destination;
 
   public function __construct() {
     parent::__construct();
@@ -13,10 +19,56 @@ class Rsync extends Shell {
     if (empty($this->rsync)) {
       throw new \Exception('Rsync not found');
     }
-
   }
 
-  public function sync($source, $destination, $flags = '-vr', $options = '') {
-    return $this->execute("$this->rsync $flags $source/ $destination/ $options");
+  /**
+   * Executes the rsync command based on class properties.
+   * @return string
+   * @throws \Exception
+   */
+  public function sync() {
+    $options = !empty($this->options) ? implode(" ", $this->options) : "";
+
+    if (empty($this->source)) {
+      throw new \Exception('No source specified for rsync');
+    }
+
+    if (empty($this->destination)) {
+      throw new \Exception('No destination specified for rsync');
+    }
+
+    return $this->execute("$this->rsync $this->flags $this->source $this->destination $options");
+  }
+
+  /**
+   * The source to copy from.
+   * @param $source
+   */
+  public function setSource($source) {
+    $this->source = $source . '/';
+  }
+
+  /**
+   * The destination to copy to.
+   * @param $destination
+   */
+  public function setDestination($destination) {
+    $this->destination = $destination . '/';
+  }
+
+  /**
+   * Paths to be excluded from the rsync command.
+   * @param $path
+   */
+  public function addExclude($path) {
+    $this->options[] = "--exclude=$path";
+  }
+
+  /**
+   * Set flags on the rsync command.
+   * @param $flags
+   */
+  public function setFlags($flags) {
+    $this->flags = '-' . $flags;
   }
 }
