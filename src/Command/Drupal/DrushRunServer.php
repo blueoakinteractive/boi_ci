42307648b1ae8780bdce98e2e9e0b33d801f6576
Andy Giles <andyg5000@gmail.com>
2017-06-06 17:18:54 -0400

    Allow url argument for drush:run-server

--- src/Command/Drupal/DrushRunServer.php
+++ src/Command/Drupal/DrushRunServer.php
@@ -4,6 +4,7 @@ namespace BOI_CI\Command\Drupal;
 
 use BOI_CI\Command\BaseCommand;
 use BOI_CI\Service\Drush as DrushService;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -13,13 +14,15 @@ class DrushRunServer extends BaseCommand
   {
     $this
       ->setName('drush:run-server')
-      ->setDescription('Use drush to run PHP server cf');
+      ->setDescription('Use drush to run PHP server cf')
+      ->addArgument('url', InputArgument::REQUIRED, 'The full url and port of the server (http://localhost:8080)');
   }
 
   protected function execute(InputInterface $input, OutputInterface $output)
   {
+    $url = $input->getArgument('url');
     $drush = new DrushService($this->build_root);
     $drush->setDir($this->build_root);
-    $drush->runServer('http://localhost:8080');
+    $drush->runServer($url);
   }
 }
