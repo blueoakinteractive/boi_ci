fe176b62ccd426ada118ba8c019ee0fdb4a187b3
Andy Giles <andyg5000@gmail.com>
2017-07-03 15:33:42 -0400

    Update Tasks.php to allow tasks defined in build as well as environment

--- src/Command/Build/Tasks.php
+++ src/Command/Build/Tasks.php
@@ -16,33 +16,50 @@ class Tasks extends BaseCommand
     $this
       ->setName('build:tasks')
       ->setDescription('Runs per-environment tasks defined in config')
-      ->addArgument('environment', InputArgument::REQUIRED, 'The environment to deploy to');
+      ->addArgument('environment', InputArgument::OPTIONAL, 'Run tasks for a specific environment. If not provided the tasks defined in build are ran.');
   }
 
   protected function execute(InputInterface $input, OutputInterface $output)
   {
-    // Make sure the required environment exists in the configuration files.
     $environment = $input->getArgument('environment');
-    if (empty($this->config['environments'][$environment])) {
-      throw new \Exception("Environment '$environment' is not defined in your config file.");
+
+    // If an environment was provided, make sure tasks
+    // exist for that environment otherwise fail.
+    if (isset($environment)) {
+      if (empty($this->config['environments'][$environment]['tasks']) || !is_array($this->config['environments'][$environment]['tasks'])) {
+        throw new \Exception("Please define the tasks for your '$environment' environment in your config file");
+      }
+
+      $tasks = $this->config['environments'][$environment]['tasks'];
     }
 
-    // Make sure a git uri is specified for provided environment.
-    if (empty($this->config['environments'][$environment]['tasks']) || !is_array($this->config['environments'][$environment]['tasks'])) {
-      throw new \Exception("Please define the tasks for your '$environment' environment in your config file");
+    // If no environment was provided determine if there are
+    // project-wide build tasks defined.
+    else {
+      if (empty($this->config['build']['tasks']) || !is_array($this->config['build']['tasks'])) {
+        $output->writeln('Skipping project build tasks since there are none defined in your config.');
+        return;
+      }
+
+      $tasks = $this->config['build']['tasks'];
+      $environment = 'all';
     }
 
-    foreach ($this->config['environments'][$environment]['tasks'] as $key => $task) {
+    // Run each defined task.
+    foreach ($tasks as $key => $task) {
+      // A working directory and command are required for each task.
       if (!isset($task['directory']) || empty($task['command'])) {
         throw new \Exception("Please define a 'directory' and 'command' for your $key task");
       }
-      $output->writeln("Executing $key task for $environment environment");
+
+      $output->writeln("Executing $key task for $environment environment(s)");
       $shell = new Shell();
       $shell->setDir($this->build_root . '/' . $task['directory']);
       $shell->setTimeout(null);
       $execute = $shell->execute($task['command']);
       $output->writeln("$execute");
     }
-    $output->writeln("Tasks for $environment environment complete");
+
+    $output->writeln("Tasks for $environment environment(s) complete");
   }
 }
