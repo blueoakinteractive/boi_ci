cbb8040c24559e030df4924c3d3effae1e7c88fd
Andy Giles <andyg5000@gmail.com>
2017-09-20 16:39:02 -0400

    Fix git commit test to use newer status of 'nothing to commit, working directory clean'

--- tests/Service/GitTest.php
+++ tests/Service/GitTest.php
@@ -62,7 +62,7 @@ class GitTest extends TestCase
     $commit_message = 'Testing commit of ' . $repo;
     $git->gitCommit($commit_message);
     $status = $git->gitStatus();
-    $this->assertContains('nothing to commit, working tree clean', $status);
+    $this->assertContains('nothing to commit, working directory clean', $status);
     $last_commit = $git->lastCommitMessage();
     $this->assertContains($commit_message, $last_commit);
     return $repo;
