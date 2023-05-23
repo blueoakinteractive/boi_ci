10d6ce57e55ce4d8e6eecd80cb9566ebd1be9ff1
Andyg5000 <andyg5000@gmail.com>
2023-05-23 17:39:38 +0000

    Execute drush from cwd

--- tests/Service/DrushTest.php
+++ tests/Service/DrushTest.php
@@ -4,13 +4,11 @@ namespace BOI_CI\Service;
 
 use PHPUnit\Framework\TestCase;
 
-define('DRUSH_TEST_TEMP_DIR_BASE', '/tmp/boi-ci-service-drush-test');
-
 class DrushTest extends TestCase
 {
   public function testDrush()
   {
-    $drush = new Drush(DRUSH_TEST_TEMP_DIR_BASE);
+    $drush = new Drush(\getcwd());
     $output = $drush->drush('status');
     $this->assertStringContainsStringIgnoringCase('Drush version', $output);
     return $drush;
